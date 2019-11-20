517f1791aa70a5ec9d9eb124879e51a1bc1d714c
Marcin Kaciuba <aldor007@gmail.com>
2019-11-20 19:00:48 +0100

    feat: added list endpoint, added validation for url

--- golang-talk-examples/crawler-step-by-step/7-image.go
+++ golang-talk-examples/crawler-step-by-step/7-image.go
@@ -3,18 +3,32 @@ package main // define package
 import (
 	"fmt"
 	"math/rand"
+	"net"
 	"sync"
+	"time"
 )                  // import necessary package to print to console
 import "net/http"  // import part of the net package to serve http requests and make requests
 import "bufio" // we'll switch from io's reader to bufio's scanner to read the body line by line
 import "os" // we'll use it to create file writers
 import "html/template"
 
-const addr = "localhost:55555" // define an address to listen on, this will not change so should be const
+const addr = "localhost:8080" // define an address to listen on, this will not change so should be const
 var urls chan string
 var links []string
 var linksLock sync.RWMutex
 var tmplt *template.Template
+
+var client = &http.Client{
+	Transport: &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	},
+}
 
 func main() { // define main function
 	urls = make(chan string, 10)
@@ -25,6 +39,7 @@ func main() { // define main function
 		fmt.Println(err)
 	}
 	http.HandleFunc("/", handle) // add a handler to the default ServeMux
+	http.HandleFunc("/list", handleList) // add a handler to the default ServeMux
 
 
 	f, err := os.OpenFile("data.txt", os.O_APPEND | os.O_CREATE | os.O_RDWR, 0666)
@@ -44,14 +59,30 @@ func main() { // define main function
 		for {
 			select {
 			case url := <- urls:
-				_, err := fmt.Fprintf(f, url + "\n")
+				req, err := http.NewRequest("HEAD", url , nil)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				res, err := client.Do(req)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+
+				if res.StatusCode > 399 {
+					fmt.Println("invalid response code", res.StatusCode, "for", url)
+					continue
+				}
+
+				_, err = fmt.Fprintf(f, url + "\n")
 				if err != nil {
 					fmt.Println(err)
 				}
 				linksLock.Lock()
 				links = append(links, url)
 				linksLock.Unlock()
-				fmt.Println("Adding", url, len(links))
+				fmt.Println("Adding", url)
 
 
 			}
@@ -89,8 +120,11 @@ func handle (w http.ResponseWriter, r *http.Request) { // define a function that
 			urls <- url
 		}
 
-		w.WriteHeader(http.StatusOK)  // write the header to the outgoing socket with 200 status code
+		w.WriteHeader(http.StatusAccepted)  // write the header to the outgoing socket with 200 status code
+		fmt.Fprintln(w, "Added to queue. You can use /list endpoint for list of supported images")
 	} else if r.Method == http.MethodGet {
+
+
 		linksLock.RLock()
 		defer linksLock.RUnlock()
 		linksLen := len(links)
@@ -110,3 +144,11 @@ func handle (w http.ResponseWriter, r *http.Request) { // define a function that
 	// response is automatically finished when the handle function returns
 }
 
+func handleList (w http.ResponseWriter, r *http.Request)  {
+	w.WriteHeader(http.StatusOK)
+	linksLock.RLock()
+	defer linksLock.RUnlock()
+	for _, link := range links {
+		fmt.Fprintln(w,  link)
+	}
+}
